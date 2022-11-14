f997c262d871f8e48d0130a24937ed623e519940
Emmanuel Chinonye Nnajiofor <emiklad1993@gmail.com>
2022-11-14 14:17:47 +0100

    added assets and starting work on navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "./navbar.module.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={`${styles.gpt3__navbar}`}>
+      <div className={styles.gpt3__navbar - links}>
+        <div className={styles.gpt3__navbar - links_logo}>
+          <img src={logo} alt="gpt3-logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
