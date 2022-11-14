359147f416ee551bc629bdcad6726367e38a82d2
Emmanuel Chinonye Nnajiofor <emiklad1993@gmail.com>
2022-11-14 14:30:37 +0100

    done with addeing asssets

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+// import logo from "logo.svg";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className={`${styles.gpt3__navbar}`}>
-      <div className={styles.gpt3__navbar - links}>
-        <div className={styles.gpt3__navbar - links_logo}>
-          <img src={logo} alt="gpt3-logo" />
+    <div className={styles.gpt3__navbar}>
+      <div className={styles["gpt3__navbar-links"]}>
+        <div className={styles["gpt3__navbar-links_logo"]}>
+          <Image src="/logo.svg" alt="gpt3-logo" width={80} height={50} />
         </div>
       </div>
     </div>
