47ecfe75eb8ccdddd3b6ad48cdc8d887cc8761b6
Emmanuel Chinonye Nnajiofor <emiklad1993@gmail.com>
2022-11-14 13:02:50 +0100

    strting out the building ofthe landing page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Head from "next/head";
-import { Article, CTA, Feature, Brand, Navbar } from "../components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "../containers";
+import { CTA, Brand, Navbar } from "../components";
+
 const Home = () => {
-  // className={`${$.card} ${classes}`}
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className={`${$.App}`}>
+      <div className={`${$.gradient__bg}`}>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
