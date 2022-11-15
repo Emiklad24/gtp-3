b46e396a724f90a68b79cffdefd3f24dfcd0a235
Emmanuel Chinonye Nnajiofor <emiklad1993@gmail.com>
2022-11-15 11:36:15 +0100

    work in progress

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -9,8 +9,31 @@ const Navbar = () => {
     <div className={styles.gpt3__navbar}>
       <div className={styles["gpt3__navbar-links"]}>
         <div className={styles["gpt3__navbar-links_logo"]}>
-          <Image src="/logo.svg" alt="gpt3-logo" width={62} height={16} />
+          <Image src="/logo.svg" alt="gpt3-logo" width={62.56} height={16.02} />
         </div>
+
+        <div className={styles["gpt3__navbar-links_container"]}>
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+
+      <div className={styles["gpt3__navbar-sign"]}>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
       </div>
     </div>
   );
